d672d7ce33e9768008f72ba79049e3a574a0db21
Gaston Soliz <gaston.e.soliz@gmail.com>
2023-12-04 15:55:05 -0300

    great update!

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,13 +1,104 @@
 import ContactButton from "../../components/ContactButton/ContactButton";
 import style from "./about.module.css";
 import experience from "../../assets/experience.png";
+import html from "../../assets/html.png";
+import css from "../../assets/css.png";
+import javascript from "../../assets/js.png";
+import react from "../../assets/react.png";
+import next from "../../assets/next.png";
+import redux from "../../assets/redux.png";
+import node from "../../assets/node.png";
+import express from "../../assets/express.png";
+import sequelize from "../../assets/sequelize.png";
+import postgresql from "../../assets/pgadmin.png";
 
 export default function About() {
   return (
     <div className={style.aboutContainer}>
       <div className={style.leftContainer}>
         <h1>Sobre mi!</h1>
-        {/* IMAGEN MIA */}
+        <div className={style.skills}>
+          <div className={style.side}>
+            <h2>Frontend Development</h2>
+            <div className={style.sideSkills}>
+              <article>
+                <img src={html} />
+                <div>
+                  <h4>HTML</h4>
+                  <small>Intermedio</small>
+                </div>
+              </article>
+              <article>
+                <img src={css} />
+                <div>
+                  <h4>CSS</h4>
+                  <small>Basico</small>
+                </div>
+              </article>
+              <article>
+                <img src={javascript} />
+                <div>
+                  <h4>Javascript</h4>
+                  <small>Intermedio</small>
+                </div>
+              </article>
+              <article>
+                <img src={react} />
+                <div>
+                  <h4>React.js</h4>
+                  <small>Intermedio</small>
+                </div>
+              </article>
+              <article>
+                <img src={redux} />
+                <div>
+                  <h4>Redux</h4>
+                  <small>Intermedio</small>
+                </div>
+              </article>
+              <article>
+                <img src={next} />
+                <div>
+                  <h4>Next.js</h4>
+                  <small>Basico</small>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className={style.side}>
+            <h2>Backend Development</h2>
+            <div className={style.sideSkills}>
+              <article>
+                <img src={node} />
+                <div>
+                  <h4>Node.js</h4>
+                  <small>Basico</small>
+                </div>
+              </article>
+              <article>
+                <img src={express} />
+                <div>
+                  <h4>Express</h4>
+                  <small>Intermedio</small>
+                </div>
+              </article>
+              <article>
+                <img src={sequelize} />
+                <div>
+                  <h4>Sequelize</h4>
+                  <small>Basico</small>
+                </div>
+              </article>
+              <article>
+                <img src={postgresql} />
+                <div>
+                  <h4>PostgreSQL</h4>
+                  <small>Basico</small>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={style.textContainer}>
         <div className={style.card}>
@@ -16,11 +107,6 @@ export default function About() {
             Experiencia: 3 proyectos finalizados e igual bajo optimizacion!
           </h2>
         </div>
-        {/* <div>
-          <img />
-          <p>Proyectos: 3 proyectos finalizados he igual bajo optimizacion!</p>
-        </div> */}
-
         <h3>
           Siendo alguien nuevo, tengo un gran interes como asi una pequeña
           ambicion para poder ser idoneo para los demas. Actualmente dedicandome
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,45 +1,28 @@
-import { useState } from "react";
 import style from "./contact.module.css";
 import email from "../../assets/email.svg";
 import linkedin from "../../assets/linkedin.svg";
 import github from "../../assets/github.svg";
 import { Link } from "react-router-dom";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    //subject: '',
-    message: "",
-  });
+  const refForm = useRef();
 
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch("/contact", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", formData }),
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
-  };
+  function sendEmail(event) {
+    event.preventDefault();
+    emailjs.sendForm(
+      "service_9o5pmio",
+      "template_11u7hla",
+      event.target,
+      "nhJeYfQ2JmdxwHwn0"
+    );
+  }
 
   return (
     <div className={style.contactContainer}>
       <div className={style.redContainer}>
-        <h1>Contact me!</h1>
+        <h1>Contactame!</h1>
         <p>
           <img src={email} />
           Email: gaston.e.soliz@gmail.com
@@ -58,13 +41,7 @@ export default function Contact() {
         </Link>
       </div>
       {/* MAPS DE QUILMES */}
-      <form
-        name="contact"
-        method="POST"
-        action="/contact"
-        className={style.formContainer}
-      >
-        <input type="hidden" name="form-name" value="contact" />
+      <form ref={refForm} onSubmit={sendEmail} className={style.formContainer}>
         <label>Email: </label>
         <input type="email" name="email" placeholder="Ingresa tu email..." />
         <label>Asunto: </label>
