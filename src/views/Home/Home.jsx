66c275e3ddaf89e2140608bc8d5c74664434c283
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-28 21:55:51 -0300

    giving id to the components

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -6,39 +6,49 @@ import me from "../../assets/ProfileW.png";
 import { useLanguage } from "../../LanguageContext";
 import SliderSkills from "../../components/Slider/SliderSkills";
 import Portfolio from "../../components/Portfolio/Portfolio";
+import Contact from "../../components/Contact/Contact";
 
 export default function Home() {
   const { lang } = useLanguage();
 
   return (
-    <div className={style.homeContainer}>
-      <div className={style.photo}>
-        <img src={me} alt="me" />
+    <>
+      <div className={style.homeContainer} id="home">
+        <div className={style.photo}>
+          <img src={me} alt="me" />
+        </div>
+        <h2>
+          {lang === "es" ? "Hola! Soy Gaston Soliz" : "Hi! I'm Gaston Soliz"}
+        </h2>
+        <h2>
+          {lang === "es" ? "Desarrollador Full Stack" : "Full Stack Developer"}
+        </h2>
+        <h1>
+          HTML | CSS | Javascript | React.JS | Redux.js | Node.js | Express.js |
+          PostgreSQL | Sequelize
+        </h1>
+        <div className={style.buttons}>
+          {lang === "es" ? (
+            <a href={CVes} download>
+              <button>Descarga mi CV!</button>
+            </a>
+          ) : (
+            <a href={CVen} download>
+              <button>Download my resume!</button>
+            </a>
+          )}
+          <ContactButton />
+        </div>
       </div>
-      <h2>
-        {lang === "es" ? "Hola! Soy Gaston Soliz" : "Hi! I'm Gaston Soliz"}
-      </h2>
-      <h2>
-        {lang === "es" ? "Desarrollador Full Stack" : "Full Stack Developer"}
-      </h2>
-      <h1>
-        HTML | CSS | Javascript | React.JS | Redux.js | Node.js | Express.js |
-        PostgreSQL | Sequelize
-      </h1>
-      <div className={style.buttons}>
-        {lang === "es" ? (
-          <a href={CVes} download>
-            <button>Descarga mi CV!</button>
-          </a>
-        ) : (
-          <a href={CVen} download>
-            <button>Download my resume!</button>
-          </a>
-        )}
-        <ContactButton />
+      <div id="skills">
+        <SliderSkills />
       </div>
-      <SliderSkills />
-      <Portfolio />
-    </div>
+      <div id="portfolio">
+        <Portfolio />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </>
   );
 }
