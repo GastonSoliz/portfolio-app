74183b611c4e03341a5c69c680deae84445b2925
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-28 21:57:35 -0300

    adding new feature

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import style from "./nav.module.css";
 import bars from "../../assets/bars-solid.svg";
 import { useState } from "react";
@@ -8,6 +8,8 @@ import { useLanguage } from "../../LanguageContext";
 export default function NavBar() {
   const [nav, setNav] = useState(false);
   const { lang } = useLanguage();
+  const location = useLocation();
+  console.log(location.pathname);
 
   function handleBar() {
     setNav(!nav);
@@ -18,28 +20,48 @@ export default function NavBar() {
       <div onClick={handleBar} className={style.navMobileClose}>
         <img src={bars} width={30} height={30} />
       </div>
-      <ul className={`${style.nav} ${nav ? style.open : ""}`}>
-        <li>
-          <Link to="/" onClick={handleBar}>
-            {lang === "es" ? "Inicio" : "Home"}
-          </Link>
-        </li>
-        <li>
-          <Link to="/work" onClick={handleBar}>
-            {lang === "es" ? "Projectos" : "Projects"}
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" onClick={handleBar}>
-            {lang === "es" ? "Sobre mi" : "About me"}
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" onClick={handleBar}>
-            {lang === "es" ? "Contacto" : "Contact"}
-          </Link>
-        </li>
-      </ul>
+      {location.pathname === "/" ? (
+        <ul className={`${style.nav} ${nav ? style.open : ""}`}>
+          <li>
+            <a href="#home" onClick={handleBar}>
+              {lang === "es" ? "Inicio" : "Home"}
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" onClick={handleBar}>
+              {lang === "es" ? "Projectos" : "Projects"}
+            </a>
+          </li>
+          <li>
+            <Link to="/about" onClick={handleBar}>
+              {lang === "es" ? "Sobre mi" : "About me"}
+            </Link>
+          </li>
+          <li>
+            <a href="#contact" onClick={handleBar}>
+              {lang === "es" ? "Contacto" : "Contact"}
+            </a>
+          </li>
+        </ul>
+      ) : (
+        <ul className={`${style.nav} ${nav ? style.open : ""}`}>
+          <li>
+            <Link to="/" onClick={handleBar}>
+              {lang === "es" ? "Inicio" : "Home"}
+            </Link>
+          </li>
+          <li>
+            <Link to="/work" onClick={handleBar}>
+              {lang === "es" ? "Projectos" : "Projects"}
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={handleBar}>
+              {lang === "es" ? "Sobre mi" : "About me"}
+            </Link>
+          </li>
+        </ul>
+      )}
       <ToggleButton />
     </div>
   );
