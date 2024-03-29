af93b02523f520582f16ee377bf3a7b84ed9b885
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-28 22:07:18 -0300

    editing text

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,8 +12,8 @@ export default function Work() {
         <h1>Portfolio</h1>
         <h2>
           {lang === "es"
-            ? "Aqui se pueden ver los proyectos en los que he trabajado por mi cuenta o como parte de un equipo. Estos fueron hechos mientras estaba aprendiendo en Soy Henry"
-            : "Here you can see the projects i have been working on my own or as part of a team. These projects were made while i was learning at Soy Henry Bootcamp"}
+            ? "Aqui se pueden ver los proyectos en los que he trabajado por mi cuenta o como parte de un equipo."
+            : "Here you can see the projects i have been working on my own or as part of a team."}
         </h2>
       </div>
       <PortfolioCardList lang={lang} />
