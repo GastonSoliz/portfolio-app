15d79f375fa4fadc9e125822db53d417ce9b5f7a
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-28 22:08:25 -0300

    adding english text

--- src/components/PortfolioCardList/PortfolioCardList.jsx
+++ src/components/PortfolioCardList/PortfolioCardList.jsx
@@ -13,21 +13,27 @@ export default function PortfolioCardList({ lang }) {
       name: "Rick & Morty App",
       images: photo0RM,
       tech: [react, node],
-      description:
-        "Aplicacion Fullstack con login, carrousel, seleccion de personajes, opcion de favoritos y filtros de busqueda",
+      descriptionES:
+        "Aplicacion Fullstack con login, carrousel, extraer personajes desde API, seleccion de favoritos y filtros de busqueda",
+      descriptionEN:
+        "Fullstack App with login, carrousel, take characters from API, favorite selection, and search filters",
     },
     {
       name: "Drivers App",
       images: photo0DR,
       tech: [react, node],
-      description: "Operaciones CRUD sobre corredores, con filtros de busqueda",
+      descriptionES:
+        "Operaciones CRUD sobre corredores, con filtros de busqueda",
+      descriptionEN: "CRUD operation about drivers with search filters",
     },
     {
       name: "Eventify App",
       images: photo0EP,
       tech: [next, node],
-      description:
-        "E-Commerce completo sobre Eventos, que incluye: Pasarela de pagos, Login, Autenticacion de usuario, Dashboard, Reviews, Filtros de busqueda",
+      descriptionES:
+        "E-Commerce completo sobre Eventos, que incluye: Pasarela de pagos, Login, Autenticacion de usuario, Panel de configuracion, Reseñas, Filtros de busqueda",
+      descriptionEN:
+        "Complete E-Commerce about Events, it includes: Payment gateway, Login, User Authentication, Dashboard, Reviews, Search filters",
     },
   ];
 
