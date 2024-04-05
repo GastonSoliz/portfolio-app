8dea97241dac8bb60ee9550237badf75116e9bdc
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-04 21:54:33 -0300

    changing workcard

--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -18,40 +18,55 @@ export default function WorkCard({ project, lang }) {
 
   return (
     <div className={style.cardContainer}>
-      <h2>{project.name}</h2>
-      <div className={style.carrouselContainer}>
-        {currentImage !== 0 && (
-          <button onClick={handlePrev} className={style.carrouselBtn}>
-            <img src={prev} />
-          </button>
-        )}
-        <img
-          src={project.images[currentImage]}
-          className={style.carrouselImg}
-        />
-        {project.images.length - 1 !== currentImage && (
-          <button onClick={handleNext} className={style.carrouselBtn}>
-            <img src={next} />
-          </button>
-        )}
+      <div className={style.leftSide}>
+        <h2>{project.name}</h2>
+        <div className={style.carrouselContainer}>
+          {currentImage !== 0 && (
+            <button onClick={handlePrev} className={style.carrouselBtn}>
+              <img src={prev} />
+            </button>
+          )}
+          <img
+            src={project.images[currentImage]}
+            className={style.carrouselImg}
+          />
+          {project.images.length - 1 !== currentImage && (
+            <button onClick={handleNext} className={style.carrouselBtn}>
+              <img src={next} />
+            </button>
+          )}
+        </div>
       </div>
-      <h4>{lang === "es" ? project.typeES : project.typeEN}</h4>
-      <div className={style.btnView}>
-        {project.gitHub && (
-          <Link to={project.gitHub}>
-            <button>
-              GitHub
-              <img src={github} width={25} height={25} />
-            </button>
-          </Link>
-        )}
-        {project.demo && (
-          <Link to={project.demo}>
-            <button>
-              Live Demo <img src={redirect} width={25} height={25} />
-            </button>
-          </Link>
-        )}
+      <div className={style.rightSide}>
+        <div className={style.topSide}>
+          <h3>
+            {lang === "es" ? project.descriptionES : project.descriptionEN}
+          </h3>
+          <h3>{lang === "es" ? project.prevES : project.prevEN}</h3>
+          {project.tech.map((t, index) => (
+            <img src={t} key={index} />
+          ))}
+        </div>
+        <div className={style.bottomSide}>
+          <h4>{lang === "es" ? project.typeES : project.typeEN}</h4>
+          <div className={style.btnView}>
+            {project.gitHub && (
+              <Link to={project.gitHub}>
+                <button>
+                  GitHub
+                  <img src={github} width={25} height={25} />
+                </button>
+              </Link>
+            )}
+            {project.demo && (
+              <Link to={project.demo}>
+                <button>
+                  Live Demo <img src={redirect} width={25} height={25} />
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
