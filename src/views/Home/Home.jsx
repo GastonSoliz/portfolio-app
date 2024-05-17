62a90b919973066dbec5b4d42fa7dadc14cfd192
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-05-17 13:15:49 -0300

    refactoring home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -14,30 +14,54 @@ export default function Home() {
   return (
     <>
       <div className={style.homeContainer} id="home">
-        <div className={style.photo}>
-          <img src={me} alt="me" />
+        <div className={style.photoContainer}>
+          <div className={style.photo}>
+            <img src={me} alt="me" />
+          </div>
         </div>
-        <h2>
-          {lang === "es" ? "Hola! Soy Gaston Soliz" : "Hi! I'm Gaston Soliz"}
+        {/* <h2>
+          {lang === "es"
+            ? "Hola! mi nombre es Gaston Soliz"
+            : "Hi! I'm Gaston Soliz"}
         </h2>
         <h2>
           {lang === "es" ? "Desarrollador Full Stack" : "Full Stack Developer"}
-        </h2>
-        <h1>
-          HTML | CSS | Javascript | React.JS | Redux.js | Node.js | Express.js |
-          PostgreSQL | Sequelize
-        </h1>
-        <div className={style.buttons}>
-          {lang === "es" ? (
-            <a href={CVes} download>
-              <button>Descarga mi CV!</button>
-            </a>
-          ) : (
-            <a href={CVen} download>
-              <button>Download my resume!</button>
-            </a>
-          )}
-          <ContactButton />
+        </h2> */}
+        <div className={style.textContainer}>
+          <span>Hola! Mi nombre es </span>
+          <h1>Gaston Soliz</h1>
+          <h2>Desarrollador Full Stack</h2>
+          <h4>
+            HTML | CSS | Javascript | React.JS | Redux.js | Node.js | Express.js
+            | PostgreSQL | Sequelize
+          </h4>
+          <p>
+            Experiencia con React (CRA / Vite.js / Next.js) en el frontend, y
+            Node.js / Express en el backend. En conjunto, me aventuro a crear
+            soluciones web que combinen la funcionalidad y el diseño ofrezco.
+          </p>
+          <p>
+            Mi objetivo es transformar ideas en posibles experiencias digitales
+            mediante soluciones creativas y eficentes, contribuyendo al progreso
+            de este ambito.
+          </p>
+          <p>
+            Comprometido con el aprendizaje continuo y evolucionar en el campo
+            del desarrollo web. Enfocado en el desarrollo de alta caldad, con un
+            enfoque especial en la formacion y el desarrollo profesional
+          </p>
+          <div className={style.buttons}>
+            {lang === "es" ? (
+              <a href={CVes} download>
+                <button>¡Descarga mi CV!</button>
+              </a>
+            ) : (
+              <a href={CVen} download>
+                <button>¡Download my resume!</button>
+              </a>
+            )}
+            <ContactButton />
+          </div>
         </div>
       </div>
       <div id="skills">
