7d45f09bbf81f964e26be4b665095ca339267879
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-06-22 17:38:49 -0300

    adding new styles at card

--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -19,6 +19,7 @@ export default function WorkCard({ project, lang }) {
   return (
     <div className={style.cardContainer}>
       <div className={style.leftSide}>
+        {/* <h3>{lang === "es" ? project.typeES : project.typeEN}</h3> */}
         <h2>{project.name}</h2>
         <div className={style.carrouselContainer}>
           {currentImage !== 0 && (
@@ -39,16 +40,24 @@ export default function WorkCard({ project, lang }) {
       </div>
       <div className={style.rightSide}>
         <div className={style.topSide}>
+            {/* <h4>{lang === "es" ? project.typeES : project.typeEN}</h4> */}
           <p>
             {lang === "es" ? project.descriptionES : project.descriptionEN}
           </p>
-          <p>{lang === "es" ? project.prevES : project.prevEN}</p>
+          {/* <p>{lang === "es" ? project.prevES : project.prevEN}</p>
           {project.tech.map((t, index) => (
             <img src={t} key={index} />
-          ))}
+          ))} */}
         </div>
         <div className={style.bottomSide}>
+          <div className={style.techSide}>
           <h4>{lang === "es" ? project.typeES : project.typeEN}</h4>
+          <div className={style.bottomTech}>
+            {project.tech.map((t, index) => (
+              <img src={t} key={index} className={style.techImg}/>
+            ))}
+          </div>
+          </div>
           <div className={style.btnView}>
             {project.gitHub && (
               <a href={project.gitHub} target="_blank">
